01dbb57b6d6cc6653c7951a07b19df83151e4cfe
yhywzh <118170286@qq.com>
2022-11-08 16:33:05 +0800

    first commit

---
+++ src/com/yhy/pojo/Main.java
@@ -0,0 +1,11 @@
+package com.yhy.pojo;
+
+/**
+ * 作者：林薛
+ * 时间：2022/11/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
